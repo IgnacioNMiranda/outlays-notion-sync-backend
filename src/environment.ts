b1f73fee96b91cf5a76855e3e328318ded7fa5e7
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-03-20 23:49:25 -0300

    feat: create outlay with year and card payment

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   notion: {
     accessToken: process.env.NOTION_ACCESS_TOKEN,
-    databaseId: process.env.NOTION_DATABASE_ID,
+
+    outlaysDatabaseId: process.env.NOTION_OUTLAYS_DATABASE_ID,
     namePropertyKey: process.env.NOTION_NAME_PROPERTY_KEY,
     datePropertyKey: process.env.NOTION_DATE_PROPERTY_KEY,
     tagsPropertyKey: process.env.NOTION_TAGS_PROPERTY_KEY,
@@ -9,5 +10,11 @@ export const environment = {
     paymentMethodPropertyKey: process.env.NOTION_PAYMENT_METHOD_PROPERTY_KEY,
     purchaseYearPropertykey: process.env.NOTION_PURCHASE_YEAR_PROPERTY_KEY,
     cardPaymentsPropertykey: process.env.NOTION_CARD_PAYMENTS_PROPERTY_KEY,
+
+    yearsDatabaseId: process.env.NOTION_YEARS_DATABASE_ID,
+    yearsNamePropertyKey: process.env.NOTION_YEARS_NAME_PROPERTY_KEY,
+
+    cardPaymentsDatabaseId: process.env.NOTION_CARD_PAYMENTS_DATABASE_ID,
+    cardPaymentsNamePropertyKey: process.env.NOTION_CARD_PAYMENTS_NAME_PROPERTY_KEY,
   },
 }
--- src/services/notion/common.ts
+++ src/services/notion/common.ts
@@ -9,15 +9,33 @@ export const createOutlay = async (data: CreateOutlayDTO): Promise<{ data: Creat
     const year = date.getFullYear()
     const monthName = date.toLocaleString('en-US', { month: 'long' })
 
-    let cardPaymentName: string
+    let cardPaymentId: string
     if (data.paymentMethod.includes('Credit')) {
       // Format: {Type} Credit {Something else}
       const creditType = data.paymentMethod.split('Credit')[0].trim()
-      cardPaymentName = `${monthName} ${year} ${creditType}`
+      const cardPaymentName = `${monthName} ${year} ${creditType}`
+      const cardPaymentRelationship = await notionClient.databases.query({
+        database_id: environment.notion.cardPaymentsDatabaseId,
+      })
+      const cardPaymentRecord = cardPaymentRelationship.results.find(
+        (result) =>
+          'properties' in result &&
+          result.properties[environment.notion.cardPaymentsNamePropertyKey].title[0].text.content === cardPaymentName,
+      )
+      if (!cardPaymentRecord) return { data: null, error: 'Card Payment not found' }
+      cardPaymentId = cardPaymentRecord.id
     }
 
+    const yearRelationship = await notionClient.databases.query({ database_id: environment.notion.yearsDatabaseId })
+    const yearRecord = yearRelationship.results.find(
+      (result) =>
+        'properties' in result &&
+        result.properties[environment.notion.yearsNamePropertyKey].title[0].text.content === `${year}`,
+    )
+    if (!yearRecord) return { data: null, error: 'Year not found' }
+
     const outlay = await notionClient.pages.create({
-      parent: { database_id: environment.notion.databaseId },
+      parent: { database_id: environment.notion.outlaysDatabaseId },
       properties: {
         [environment.notion.namePropertyKey]: {
           title: [
@@ -41,15 +59,11 @@ export const createOutlay = async (data: CreateOutlayDTO): Promise<{ data: Creat
           select: { name: data.paymentMethod },
         },
         [environment.notion.purchaseYearPropertykey]: {
-          database_id: '',
-          synced_property_name: `${year}`,
-          synced_property_id: '',
+          relation: [{ id: yearRecord.id }],
         },
-        ...(cardPaymentName && {
+        ...(cardPaymentId && {
           [environment.notion.cardPaymentsPropertykey]: {
-            database_id: '',
-            synced_property_name: '',
-            synced_property_id: '',
+            relation: [{ id: cardPaymentId }],
           },
         }),
       },
